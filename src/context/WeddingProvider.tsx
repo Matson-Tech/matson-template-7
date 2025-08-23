1e3eb0e8c3bda4d8e72c6eeda22ed57978d3425a
TheBattleDog <krishnanand7777777@gmail.com>
2025-08-23 12:48:59 +0530

    test (no isLoggedIn)

--- src/context/WeddingProvider.tsx
+++ src/context/WeddingProvider.tsx
@@ -286,7 +286,7 @@ export const WeddingProvider: React.FC<{ children: React.ReactNode }> = ({
             isMounted = false;
             subscription.unsubscribe();
         };
-    }, [user?.username, user?.id, navigate, isLoggedIn]);
+    }, [user?.username, user?.id, navigate]);
 
     useEffect(() => {
         if (documentTitle) {
